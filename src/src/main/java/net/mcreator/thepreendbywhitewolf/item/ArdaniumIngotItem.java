57437e2db79ffdb03255b0d1bbb647af8cf87319
emgeorgat <137608142+ManolisG11@users.noreply.github.com>
2023-07-18 10:56:51 +0300

    Add files via upload

---
+++ src/src/main/java/net/mcreator/thepreendbywhitewolf/item/ArdaniumIngotItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.thepreendbywhitewolf.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class ArdaniumIngotItem extends Item {
+	public ArdaniumIngotItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
